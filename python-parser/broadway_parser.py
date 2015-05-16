89658954c54462f4de53c95f30c0e734132fd771
Matt Allen <matt_allen@utexas.edu>
2015-05-15 21:11:01 -0500

    Added preprocessing to the broadway parser.

--- python-parser/broadway_parser.py
+++ python-parser/broadway_parser.py
@@ -6,18 +6,26 @@ import argparse
 import sys
 import os
 import json
+import subprocess
 
 def main(options):
     parser = BroadwayParser(parseinfo=True, semantics=Semantics())
-    with open(options.input_file) as f:
-        buffer = Buffer(f.read(), filename=options.input_file, trace=True)
-        ast = parser.parse(buffer, 'start', trace=options.trace)
-        output_file = options.output_file
-        if not output_file:
-            output_file = os.path.splitext(os.path.basename(options.input_file))[0] + '.json'
-        with open(output_file, 'w') as out:
-            text = json.dumps(ast, indent=2)
-            out.write(text)
+
+    buffer = ''
+    if options.preprocess:
+        buffer = subprocess.check_output(['clang', '-E', '-P', '-x', 'c', options.input_file])
+    else:
+        with open(options.input_file) as input:
+            buffer = input.read()
+
+    ast = parser.parse(buffer, 'start', trace=options.trace)
+
+    output_file = options.output_file
+    if not output_file:
+        output_file = os.path.splitext(os.path.basename(options.input_file))[0] + '.json'
+    with open(output_file, 'w') as out:
+        text = json.dumps(ast, indent=2)
+        out.write(text)
         print(text)
 
 if __name__ == '__main__':
@@ -25,4 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('input_file')
     parser.add_argument('--trace', action='store_true')
     parser.add_argument('-o', '--output', dest='output_file')
+    parser.add_argument('-bc', '--bytecode', dest='bytecode_file')
+    parser.add_argument('--no-preprocess', action='store_false', dest='preprocess')
     main(parser.parse_args())
+
