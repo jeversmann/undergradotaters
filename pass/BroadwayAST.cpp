be3a2701d655468ac139a3554ee5c2c53705c2cf
Matt Allen <matt_allen@utexas.edu>
2015-05-16 23:40:39 -0500

    Added classes for the members of procedure.

--- pass/BroadwayAST.cpp
+++ pass/BroadwayAST.cpp
@@ -6,6 +6,7 @@ BroadwayProcedure::BroadwayProcedure(const jsValue &procedure) {
   assert(procedure.IsObject());
   const jsValue &nameV = procedure["name"];
   name = nameV.GetString();
+
   const jsValue &args = procedure["arguments"];
   assert(args.IsArray());
   for (jsValue::ConstValueIterator argIter = args.Begin();
@@ -14,60 +15,12 @@ BroadwayProcedure::BroadwayProcedure(const jsValue &procedure) {
     arguments.push_back(arg.GetString());
   }
 
-  const jsValue &exits = procedure["exit_pointers"];
-  assert(exits.IsArray());
-  for (jsValue::ConstValueIterator exitIter = exits.Begin();
-       exitIter != exits.End(); ++exitIter) {
-    const jsValue &exit = *exitIter;
-    exitPointers.push_back(&exit);
-  }
-
-  const jsValue &entries = procedure["entry_pointers"];
-  assert(entries.IsArray());
-  for (jsValue::ConstValueIterator entryIter = entries.Begin();
-       entryIter != entries.End(); ++entryIter) {
-    const jsValue &entry = *entryIter;
-    entryPointers.push_back(&entry);
-  }
-
-  const jsValue &analysesV = procedure["analyses"];
-  assert(analysesV.IsArray());
-  for (jsValue::ConstValueIterator analysisIter = analysesV.Begin();
-       analysisIter != analysesV.End(); ++analysisIter) {
-    const jsValue &analysis = *analysisIter;
-    analyses.push_back(&analysis);
-  }
-
-  const jsValue &accessesV = procedure["accesses"];
-  assert(accessesV.IsArray());
-  for (jsValue::ConstValueIterator accessIter = accessesV.Begin();
-       accessIter != accessesV.End(); ++accessIter) {
-    const jsValue &access = *accessIter;
-    accesses.push_back(&access);
-  }
-
-  const jsValue &modifiesV = procedure["modifies"];
-  assert(modifiesV.IsArray());
-  for (jsValue::ConstValueIterator modifyIter = modifiesV.Begin();
-       modifyIter != modifiesV.End(); ++modifyIter) {
-    const jsValue &modify = *modifyIter;
-    modifies.push_back(&modify);
-  }
-
-  const jsValue &reportsV = procedure["reports"];
-  assert(reportsV.IsArray());
-  for (jsValue::ConstValueIterator reportIter = reportsV.Begin();
-       reportIter != reportsV.End(); ++reportIter) {
-    const jsValue &report = *reportIter;
-    reports.push_back(&report);
-  }
-
-  const jsValue &actionsV = procedure["actions"];
-  assert(actionsV.IsArray());
-  for (jsValue::ConstValueIterator actionIter = actionsV.Begin();
-       actionIter != actionsV.End(); ++actionIter) {
-    const jsValue &action = *actionIter;
-    actions.push_back(&action);
-  }
+  addArrayOfValues(procedure["exit_pointers"], exitPointers);
+  addArrayOfValues(procedure["entry_pointers"], entryPointers);
+  addArrayOfValues(procedure["analyses"], analyses);
+  addArrayOfValues(procedure["accesses"], accesses);
+  addArrayOfValues(procedure["modifies"], modifies);
+  addArrayOfValues(procedure["reports"], reports);
+  addArrayOfValues(procedure["actions"], actions);
 }
 }
--- pass/BroadwayAST.h
+++ pass/BroadwayAST.h
@@ -5,21 +5,75 @@
 namespace dataflow {
 using namespace rapidjson;
 
-class BroadwayProcedure {
+class BroadwayBase {
+public:
   using jsValue = rapidjson::Value;
+  template <class V>
+  void addArrayOfValues(const jsValue &array, std::vector<V> &things) {
+    assert(array.IsArray());
+    for (jsValue::ConstValueIterator iter = array.Begin(); iter != array.End();
+         ++iter) {
+      const jsValue &value = *iter;
+      things.push_back(V(value));
+    }
+  }
+};
 
+class BroadwayExitPointer : public BroadwayBase {
+public:
+  BroadwayExitPointer(){};
+  BroadwayExitPointer(const jsValue &){};
+};
+
+class BroadwayEntryPointer : public BroadwayBase {
+public:
+  BroadwayEntryPointer(){};
+  BroadwayEntryPointer(const jsValue &){};
+};
+
+class BroadwayAnalysis : public BroadwayBase {
+public:
+  BroadwayAnalysis(){};
+  BroadwayAnalysis(const jsValue &){};
+};
+
+class BroadwayAccess : public BroadwayBase {
+public:
+  BroadwayAccess(){};
+  BroadwayAccess(const jsValue &){};
+};
+
+class BroadwayModify : public BroadwayBase {
+public:
+  BroadwayModify(){};
+  BroadwayModify(const jsValue &){};
+};
+
+class BroadwayReport : public BroadwayBase {
+public:
+  BroadwayReport(){};
+  BroadwayReport(const jsValue &){};
+};
+
+class BroadwayAction : public BroadwayBase {
+public:
+  BroadwayAction(){};
+  BroadwayAction(const jsValue &){};
+};
+
+class BroadwayProcedure : public BroadwayBase {
 public:
   std::string name;
   std::vector<std::string> arguments;
-  std::vector<const jsValue *> exitPointers;
-  std::vector<const jsValue *> entryPointers;
-  std::vector<const jsValue *> analyses;
-  std::vector<const jsValue *> accesses;
-  std::vector<const jsValue *> modifies;
-  std::vector<const jsValue *> reports;
-  std::vector<const jsValue *> actions;
+  std::vector<BroadwayExitPointer> exitPointers;
+  std::vector<BroadwayEntryPointer> entryPointers;
+  std::vector<BroadwayAnalysis> analyses;
+  std::vector<BroadwayAccess> accesses;
+  std::vector<BroadwayModify> modifies;
+  std::vector<BroadwayReport> reports;
+  std::vector<BroadwayAction> actions;
 
-  BroadwayProcedure();
+  BroadwayProcedure(){};
   BroadwayProcedure(std::string name) : name(name){};
   BroadwayProcedure(const jsValue &);
 };
